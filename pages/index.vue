bcc470ee90d05ec0fa4c13dd045798e9303d5907
PierreAnders <pierre.untas@gmail.com>
2023-09-10 16:21:59 +0200

    registration in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,60 @@
 <template>
+  <div>
+    <h1>Inscription</h1>
+    <form @submit.prevent="register">
+      <div>
+        <label for="username">Nom d'utilisateur:</label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" />
+      </div>
+      <div>
+        <label for="password">Mot de passe:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <div>
+        <button type="submit">S'inscrire</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async register() {
+      // Envoyez la demande d'inscription à l'API Flask avec Axios
+      try {
+        const response = await axios.post('http://localhost:5000/register', {
+          nom: this.username,
+          email: this.email,
+          mot_de_passe: this.password,
+        })
+
+        // Redirigez l'utilisateur vers une page de connexion après l'inscription réussie
+        // Vous pouvez également afficher un message de confirmation ici
+      } catch (error) {
+        console.error('Erreur d\'inscription:', error)
+      }
+    },
+  },
+}
+</script>
+
+
+
+<!-- <template>
     <div class="bg-image bg-neutral-900 px-8 pt-12 min-h-screen">
       <BurgerMenu />
     </div>
@@ -23,4 +79,4 @@
     min-height: 100vh; 
   }
   </style>
-  
+   -->
---
+++ pages/login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <h1>Connexion</h1>
+      <form @submit.prevent="login">
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" />
+        </div>
+        <div>
+          <label for="password">Mot de passe:</label>
+          <input type="password" id="password" v-model="password" />
+        </div>
+        <div>
+          <button type="submit">Se connecter</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+      }
+    },
+    methods: {
+      async login() {
+        // Envoyez la demande de connexion à l'API Flask avec Axios
+        try {
+          const response = await axios.post('http://localhost:5000/login', {
+            email: this.email,
+            mot_de_passe: this.password,
+          })
+          // TODO 
+          // Stocker le jeton d'authentification (response.data.access_token) dans le stockage local ou un cookie sécurisé
+          // Rediriger l'utilisateur vers une page protégée après la connexion réussie
+        } catch (error) {
+          console.error('Erreur de connexion:', error)
+        }
+      },
+    },
+  }
+  </script>
+  
