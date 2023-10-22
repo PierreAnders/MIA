763305975d9c96318c6311e07cc3ba9ab622e6ff
PierreAnders <pierre.untas@gmail.com>
2023-10-22 08:26:10 +0200

    add css expenses page

--- pages/finance/expenses.vue
+++ pages/finance/expenses.vue
@@ -7,52 +7,53 @@
         </div>
         <form @submit.prevent="submitExpenseInfo">
             <div class="flex flex-col items-center justify-center mt-6">
-                <div class="py-3 flex flex-col">
-                    <label class="text-gray-300" for="title">Titre</label>
+                <div class="pb-4 flex flex-col w-72">
+                    <label class="sr-only" for="title">Titre</label>
                     <input
-                        class="text-right w-64 h-8 px-4 border rounded-md focus:outline-none focus:border-amber-800 opacity-50"
-                        type="text" id="title" v-model="expenseInfo.title" placeholder="titre">
+                        class="text-white text-sm bg-dark-gray placeholder-light-gray w-72 h-8 px-4 border-2 border-dark-gray rounded-md focus:outline-none focus:border-blue"
+                        type="text" id="title" v-model="expenseInfo.title" placeholder="Titre :">
                 </div>
-                <div class="py-3 flex flex-col">
-                    <label class="text-gray-300" for="blood_group">Description</label>
-                    <input
-                        class="text-right w-64 h-8 px-4 border rounded-md focus:outline-none focus:border-amber-800 opacity-50"
-                        type="text" id="description" v-model="expenseInfo.description" placeholder="Description">
+                <div class="pb-4 flex flex-col w-72">
+                    <label class="sr-only" for="blood_group">Description</label>
+                    <textarea
+                        class="text-white text-sm bg-dark-gray placeholder-light-gray w-72 h-20 pt-1 px-4 border-2 border-dark-gray rounded-md focus:outline-none focus:border-blue"
+                        type="text" id="description" v-model="expenseInfo.description"
+                        placeholder="Description :"></textarea>
                 </div>
-                <div class="py-3 flex flex-col">
-                    <label class="text-gray-300" for="doctor">Prix</label>
-                    <input
-                        class="text-right w-64 h-8 px-4 border rounded-md focus:outline-none focus:border-amber-800 opacity-50"
-                        type="text" id="doctor" v-model="expenseInfo.price" placeholder="Prix">
-                </div>
-                <button
-                    class="flex items-center h-8 bg-slate-500 text-white mt-4 px-4 py-2 rounded-md hover:bg-slate-600 transition duration-300"
-                    type="submit">Ajouter
-                </button>
-                <div class="mb-24">
-                    <div class="text-gray-300 font-semibold flex justify-center mt-12 mb-6">TOTAL : {{ total.toFixed(2) }}
+                <div class="pb-4 flex flex-col w-72">
+                    <div class="flex">
+                        <label class="sr-only" for="doctor">Prix</label>
+                        <input
+                            class="text-white text-sm bg-dark-gray placeholder-light-gray w-72 h-8 mr-2 px-4 border-2 border-dark-gray rounded-md focus:outline-none focus:border-blue"
+                            type="text" id="doctor" v-model="expenseInfo.price" placeholder="Prix :">
+                        <button type="submit">
+                            <IconEnter />
+                        </button>
                     </div>
-                    <table class="w-full border-collapse">
-                        <thead>
-                            <tr>
-                                <th class="text-left text-amber-800 px-4 py-2">Titre</th>
-                                <th class="text-left text-amber-800 px-4 py-2">Description</th>
-                                <th class="text-left text-amber-800 px-4 py-2">Prix (€)</th>
-                                <th class="text-left text-amber-800 px-4 py-2">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="expense in expenses" :key="expense.id">
-                                <td class="border border-gray-600 text-gray-300 px-4 py-2">{{ expense.title }}</td>
-                                <td class="border border-gray-600 text-gray-300 px-4 py-2">{{ expense.description }}</td>
-                                <td class="border border-gray-600 text-gray-300 px-4 py-2">{{ expense.price }}</td>
-                                <td class="border border-gray-500 px-4 py-2">
-                                    <button @click="deleteExpense(expense.id)"
-                                        class="text-amber-800 hover:text-amber-600 transition duration-300 ease-in-out">Supprimer</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                </div>
+                <div class="mb-24 w-full  md:w-3/4 lg:w-2/3">
+                    <div class="flex my-6">
+                        <div class="text-light-gray font-semibold flex mr-2">TOTAL :</div>
+                        <div class="text-white"> {{ total.toFixed(2) }} €</div>
+                    </div>
+
+                    <div v-for="expense in expenses" :key="expense.id">
+                        <div class="mt-5">
+                            <div class="flex mt-1">
+                                <div class="text-light-gray mr-2">Titre:</div>
+                                <div class="text-white">{{ expense.title }}</div>
+                            </div>
+                            <div class="flex mt-1">
+                                <div class="text-light-gray mr-2">Prix:</div>
+                                <div class="text-white">{{ expense.price }} €</div>
+                            </div>
+                            <div class="flex mt-1">
+                                <div class="flex text-light-gray text-sm mr-2">Description:</div>
+                                <div class="text-white text-sm">{{ expense.description }}</div>
+                            </div>
+                            <button class="mt-2" @click="deleteExpense(expense.id)"><IconSubmenuDeleteFolder class="w-5 h-5" color="#553348"/></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
@@ -60,13 +61,15 @@
 </template>
     
 <script>
+import IconSubmenuDeleteFolder from '@/components/IconSubmenuDeleteFolder.vue';
 import axios from 'axios';
 import BurgerMenu from '~/components/BurgerMenu.vue';
 
 export default {
     components: {
-        BurgerMenu,
-    },
+    BurgerMenu,
+    IconSubmenuDeleteFolder,
+},
 
     data() {
         return {
