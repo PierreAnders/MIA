d468b1add2a5e4a6df799587262eb95735ac119b
PierreAnders <pierre.untas@gmail.com>
2023-11-04 22:37:10 +0100

    clean code finance-menu

--- pages/finance-menu.vue
+++ pages/finance-menu.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="min-h-screen px-8 pt-8">
         <BurgerMenu />
-        <div class="flex justify-center items-center pt-8">
-            <h1 class="text-light-gray tracking-wider pr-3">FINANCE</h1>
+        <div class="flex items-center justify-center pt-8">
+            <h1 class="pr-3 tracking-wider text-light-gray">FINANCE</h1>
             <IconFinance :color="'#334155'" />
         </div>
         <div class="flex flex-col items-center justify-center mt-12">
-            <div class="flex justify-between items-center text-white text-sm bg-dark-gray placeholder-light-gray w-72 h-8 px-4 mb-4 rounded-md">
+            <div
+                class="flex items-center justify-between h-8 px-4 mb-4 text-sm text-white rounded-md bg-dark-gray placeholder-light-gray w-72">
                 <div class="flex space-x-3">
                     <IconBalance color="#838383" />
                     <div class="text-light-gray">Balances</div>
                 </div>
                 <div>{{ (totalIncomes - totalExpenses).toFixed(2) }} €</div>
             </div>
-            <div class="flex space-x-2 w-72 pb-2">
+            <div class="flex pb-2 space-x-2 w-72">
                 <nuxt-link
-                    class="flex justify-between items-center text-white text-sm bg-dark-gray hover:bg-opacity-75 w-72 transition duration-300 h-8 px-4 mb-2 rounded-md"
+                    class="flex items-center justify-between h-8 px-4 mb-2 text-sm text-white transition duration-300 rounded-md bg-dark-gray hover:bg-opacity-75 w-72"
                     to="/finance/expenses">
                     <div class="flex space-x-3">
                         <IconExpenses color="#838383" />
@@ -27,9 +28,9 @@
                     <IconEnter class="transition-transform transform hover:scale-110" />
                 </nuxt-link>
             </div>
-            <div class="flex space-x-2 w-72 pb-2">
+            <div class="flex pb-2 space-x-2 w-72">
                 <nuxt-link
-                    class="flex justify-between items-center text-white text-sm bg-dark-gray hover:bg-opacity-75 w-72 transition duration-300 h-8 px-4 mb-2 rounded-md"
+                    class="flex items-center justify-between h-8 px-4 mb-2 text-sm text-white transition duration-300 rounded-md bg-dark-gray hover:bg-opacity-75 w-72"
                     to="/finance/incomes">
                     <div class="flex space-x-3">
                         <IconExpenses class="transform scale-x-[-1]" color="#838383" />
@@ -47,7 +48,7 @@
     
 <script>
 import axios from 'axios';
-import {BASE_URL} from '../constants.js'
+import { BASE_URL } from '../constants.js'
 
 export default {
     data() {
@@ -64,73 +65,72 @@ export default {
             },
         };
     },
-    setup() {
-        definePageMeta({
-        middleware: ['auth'],
-        });
-    },
+   
     methods: {
         async getAllIncomes() {
             try {
-                const token = localStorage.getItem("access_token");
-
-                if (!token) {
-                    console.error("Jeton JWT non trouvé.");
-                    return;
-                }
+                const token = this.getJwtToken()
 
                 const headers = {
                     Authorization: `Bearer ${token}`
                 };
 
-                const response = await axios.get(`${BASE_URL}/incomes`, { headers });
+                const response = await axios.get(`${BASE_URL}/incomes`, { headers })
 
                 if (response.status === 200) {
                     this.incomes = response.data;
-                    this.incomes.sort((a, b) => b.price - a.price);
-                    this.totalIncomes = this.calculateTotalIncomes();
+                    this.incomes.sort((a, b) => b.price - a.price)
+                    this.totalIncomes = this.calculateTotal(this.incomes)
                 } else {
-                    console.error("Échec de la récupération des recettes.");
+                    console.error("Échec de la récupération des recettes.")
                 }
             } catch (error) {
-                console.error("Erreur lors de la récupération des recettes :", error);
+                console.error("Erreur lors de la récupération des recettes :", error)
             }
-        },
-
-        calculateTotalIncomes() {
-            return this.incomes.reduce((total, income) => total + parseFloat(income.price), 0);
         },
 
         async getAllExpenses() {
             try {
-                const token = localStorage.getItem("access_token");
-
-                if (!token) {
-                    console.error("Jeton JWT non trouvé.");
-                    return;
-                }
+                const token = this.getJwtToken()
 
                 const headers = {
                     Authorization: `Bearer ${token}`
                 };
 
-                const response = await axios.get(`${BASE_URL}/expenses`, { headers });
+                const response = await axios.get(`${BASE_URL}/expenses`, { headers })
 
                 if (response.status === 200) {
                     this.expenses = response.data;
-                    this.expenses.sort((a, b) => b.price - a.price);
-                    this.totalExpenses = this.calculateTotalExpenses();
+                    this.expenses.sort((a, b) => b.price - a.price)
+                    this.totalExpenses = this.calculateTotal(this.expenses)
                 } else {
-                    console.error("Échec de la récupération des dépenses.");
+                    console.error("Échec de la récupération des dépenses.")
                 }
             } catch (error) {
-                console.error("Erreur lors de la récupération des dépenses :", error);
+                console.error("Erreur lors de la récupération des dépenses :", error)
             }
         },
 
-        calculateTotalExpenses() {
-            return this.expenses.reduce((total, expense) => total + parseFloat(expense.price), 0);
+        calculateTotal(elements) {
+            return elements.reduce((total, element) => total + parseFloat(element.price), 0)
         },
+
+        getJwtToken() {
+            const jwtToken = localStorage.getItem('access_token')
+
+            if (!jwtToken) {
+                console.error('Le jeton JWT n\'est pas disponible.')
+                this.$router.push('/')
+                return;
+            }
+            return jwtToken;
+        },
+    },
+
+    setup() {
+        definePageMeta({
+            middleware: ['auth'],
+        });
     },
 
     created() {
