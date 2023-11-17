8fd5032a90db01f46ddebdc059e70d409d6f0a95
Pierre Untas <104831958+PierreAnders@users.noreply.github.com>
2023-11-17 12:25:33 +0100

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:5000/"
+export const BASE_URL="https://3b40w1s5-5000.uks1.devtunnels.ms/"
 // export const BASE_URL="https://bxsvc05b-5000.uks1.devtunnels.ms/"
