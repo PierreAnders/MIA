6b65a4206a61fcdc99382611c6c57c877df5f974
PierreAnders <pierre.untas@gmail.com>
2023-12-05 09:38:15 +0100

    delete constants.js

--- constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL="http://localhost:5000/"
-// export const BASE_URL="https://bxsvc05b-5000.uks1.devtunnels.ms/"
-export const TINYMCE_API_KEY="87aq7pxq6hq3w41qkqqk8c7hal7tz5iv91qj678bp1m58ba7"
